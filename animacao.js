db5ebcb9eefd7180fa8a7e6d152fe6ddb9291314
vitor <vitoradler5@gmail.com>
2016-07-07 16:18:39 -0300

    inicial

---
+++ animacao.js
@@ -0,0 +1,25 @@
+function move() {
+
+	var elem = document.getElementById("myAnimation");
+	var pos = 0;
+	var id = setInterval(frame,10);
+
+	function frame() {
+
+		if (pos == 350) {
+
+			clearInterval(id);
+
+		}
+
+		else {
+
+			pos++;
+			elem.style.top = pos + 'px';
+			elem.style.left = pos + 'px';
+
+		}
+
+	}
+
+}
